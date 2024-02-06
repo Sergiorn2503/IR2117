2f48c1d1a81030d49ce2daf96cc3fa6f76a214d2
Sergio R <al428707@uji.es>
2024-02-06 11:46:37 +0100

    V5

--- ecuación_cuadratica.cc
+++ ecuación_cuadratica.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <complex>
 
 int discriminante(int a, int b, int c){
 	int disc = b*b -4*a*c;
@@ -21,12 +22,14 @@ int main() {
         int disc = discriminante(a,b,c);
 
         if(disc < 0){
-		std::cout << "No existen soluciones reales";
+		std::complex<float> raiz1(-b / (2*a), std::sqrt(-disc) / (2*a));
+        std::complex<float> raiz2(-b / (2*a), -std::sqrt(-disc) / (2*a));
+        std::cout << "Las soluciones complejas son: " << raiz1 << "y" << raiz2;
 
         }else if(disc == 0){
 		int sol1 = -b/2*a;
 		std::cout << "Existe una solución: " << sol1;
-        
+
         }else{
             int raiz = std::sqrt(disc);
             int sol1 = -b+raiz/2*a;
