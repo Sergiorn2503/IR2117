d0ba055fb977e4ffd5142905f4dda688cd35b2a3
Sergio R <al428707@uji.es>
2024-02-06 11:35:50 +0100

    V1

---
+++ ecuación_cuadratica.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+        int a;
+        int b;
+        int c;
+        std::cout << "Introduce un coeficiente de x²";
+        std::cin >> a;
+		std::cout << "Introduce un coeficiente de x¹";
+        std::cin >> b;
+        std::cout << "Introduce un coeficiente de x⁰";
+        std::cin >> c;
+        
+    int sol1 = 0;
+	int sol2 = 0;
+
+		std::cout << "Existen dos soluciones:" << sol1 << sol2;
+	
+	return 0;
+}
+
