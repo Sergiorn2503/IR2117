5b37951d3472c966f1b96567294922d99310be67
kerricaverga <al428707@uji.es>
2024-02-20 13:03:04 +0100

    seguir clase

--- ros2_ws/src/stats/src/median.cpp
+++ ros2_ws/src/stats/src/median.cpp
@@ -1,25 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include <iostream>
+#include <vector>
 
-int sum;
-int mensajes = 0;
-int mean;
+std::vector<int> vect;
 
 std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
-    sum += msg->data;
+
+    vect.push_back(msg -> data);
     std_msgs::msg::Int32 out_msg;
-    out_msg.data = sum;
     publisher -> publish(out_msg);
-    mensajes += 1;
+    for(int i = 0; i < vect.len(); i++){
+    	std::cout << vect[i] << std::endl;
+    }
 }
 
 int main(int argc, char * argv[])
 {
-    sum = 0;
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("median");
     auto subscription = 
@@ -28,8 +28,5 @@ int main(int argc, char * argv[])
     publisher = node->create_publisher<std_msgs::msg::Int32>("median", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
-    mean = sum / mensajes;
-    std::cout << mensajes << std::endl;
-    std::cout << mean << std::endl;
     return 0;
 }
