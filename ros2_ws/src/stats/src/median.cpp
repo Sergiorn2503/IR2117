d93936245e4c3ced38d133b6e7a451142e7f2127
Sergio R <al428707@uji.es>
2024-02-20 21:08:11 +0100

    revisar enric

--- ros2_ws/src/stats/src/median.cpp
+++ ros2_ws/src/stats/src/median.cpp
@@ -3,28 +3,27 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> vect;
-
 std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
-
     vect.push_back(msg -> data);
     std_msgs::msg::Int32 out_msg;
-    publisher -> publish(out_msg);
-    for(int i = 0; i < vect.len(); i++){
+    out_msg.data = msg->data;
+    //publisher -> publish(out_msg);
+    
+    for(int i = 0; i < vect.size(); i++){
     	std::cout << vect[i] << std::endl;
     }
 }
 
 int main(int argc, char * argv[])
 {
+    std::vector<int> vect = {};
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("median");
     auto subscription = 
     	node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-    
     publisher = node->create_publisher<std_msgs::msg::Int32>("median", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
