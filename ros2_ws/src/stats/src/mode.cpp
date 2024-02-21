142279713d0fbe69c917941999105aaa6f395a8c
Sergio R <al428707@uji.es>
2024-02-21 18:02:44 +0100

    seguir en clase

--- ros2_ws/src/stats/src/mode.cpp
+++ ros2_ws/src/stats/src/mode.cpp
@@ -1,4 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/Int32MultiArray.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include <iostream>
 #include <vector>
@@ -7,7 +8,7 @@
 #include <map>
 
 
-std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
+std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32MultiArray> > publisher;
 std::vector<int> vect = {};
 std::map<int, int> map;
 
@@ -16,18 +17,22 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
     vect.push_back(msg -> data);
     map[msg -> data]++;
     
-    std_msgs::msg::Int32 out_msg;
+    std_msgs::msg::Int32MultiArray out_msg;
     int mode;
     int max = 0;
     
     for(auto &pair : map) {
         if(max < pair.second){
-        	mode = pair.first;
         	max = pair.second;
         }
     }
     
-    out_msg.data = mode;
+    for(auto &pair : map) {
+        if(pair.second == max){
+            out_msg.data.push_back(pair.first);
+        }
+    }
+    
     publisher -> publish(out_msg);    
 }
 
@@ -37,7 +42,7 @@ int main(int argc, char * argv[])
     auto node = rclcpp::Node::make_shared("mode");
     auto subscription = 
     	node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-    publisher = node->create_publisher<std_msgs::msg::Int32>("mode", 10);
+    publisher = node->create_publisher<std_msgs::msg::Int32MultiArray>("mode", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
