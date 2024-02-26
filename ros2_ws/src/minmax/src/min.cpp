d81e8f4176a6ffba414c6a6bea035d3a961a10c7
kerricaverga <al428707@uji.es>
2024-02-26 11:01:04 +0100

    create min node

---
+++ ros2_ws/src/minmax/src/min.cpp
@@ -0,0 +1,31 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
+int min = 0;
+
+void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
+{
+    if(min < msg->data){
+    	min = msg->data;
+    }
+    
+    std_msgs::msg::Int32 out_msg;
+    out_msg.data = min;
+    publisher -> publish(out_msg);
+    
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("min");
+    auto subscription = 
+    	node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
+    publisher = node->create_publisher<std_msgs::msg::Int32>("min", 10);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
