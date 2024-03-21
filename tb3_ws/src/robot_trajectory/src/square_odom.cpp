cf34fa8e2cc9cddaf9063599736b780ed5a14223
kerricaverga <al428707@uji.es>
2024-03-21 12:32:18 +0100

    Create subscription to topic odom

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -1,16 +1,26 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include <cmath>
 
 using namespace std::chrono_literals;
 
+double global_x; double global_y; 
+
+void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
+	global_x = msg->pose.pose.position.x;
+	global_y = msg->pose.pose.position.y;
+	RCLCPP_INFO(rclcpp::get_logger("odom_listener"), "Position: (%f,%f)", global_x, global_y);
+}
+
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("square_odom");
+    auto node = rclcpp::Node::make_shared("cmd_vel");
     auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    
+    auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("/odom",10,odom_callback);
+	
     node->declare_parameter("linear_speed",0.1);
     node->declare_parameter("angular_speed",M_PI / 20);
     node->declare_parameter("square_length",1.0);
@@ -52,6 +62,7 @@ int main(int argc, char * argv[])
 	    publisher->publish(message);
     }
     
+	rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
